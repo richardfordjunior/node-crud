19dd9deef6b710ff200df76299ebd779a82142fc
richardfordjunior <richardfordjr@gmail.com>
2020-09-10 13:08:40 -0400

    Updating unit tests

--- test/book.controller.test.js
+++ test/book.controller.test.js
@@ -4,6 +4,7 @@ const server = require('./../server').server;
 const request = require("supertest").agent(server);
 const chai = require('chai')
 const expect = chai.expect;
+const assert = require('assert');
 
 after(done => {
   server.close();
@@ -20,107 +21,88 @@ describe("#Books API", () => {
     expect(2).eql(2)
   })
 
-  it("should return the books list", (done) => {
+  it("should return the books list", async () => {
     const body = {
       book: "My new book 15"
     }
-    request
+    const res = await request
       .post('/api/book')
       .send(body)
-      .then(res => {
-        if (res.body.data) {
-          request
-            .get('/api/books')
-            .expect(200)
-            .then(response => {
-              expect(response.body.status).toBe('Ok');
-              expect(response.data).to.eql('My new book 15');
-            })
-        }
-      })
-    done()
+
+    if (res.body.data) {
+      const response = await request
+        .get('/api/books')
+        .expect(200)
+      expect(response.body.status).to.eql('Ok');
+      expect(response.body.data).to.eql('My new book 15');
+      expect(response.body.data).to.not.eql('My new book 1');
+
+    }
   });
 
 
-  it("should raise an error when a selected book for deletion does not exist", (done) => {
+  it("should raise an error when a selected book for deletion does not exist", async () => {
     let body = { book: 'Test book 15' }
-    request
+    const resp = await request
       .delete('/api/book')
       .send(body)
-      .then((resp) => {
-        expect(response.body.status).toBe('Not Ok');
-        expect(response.body.message).toBe('The book selected does not exist!');
-      })
-    done();
+    expect(resp.body.status).to.eql('Not Ok');
+    expect(resp.body.message).to.eql('The book selected does not exist!');
   });
 
 
-  it("should delete a book from the books list", (done) => {
+  it("should delete a book from the books list", async () => {
     const body = {
-      book: "My new book 15"
+      book: "My new book 1"
     }
     const deleteBody = {
-      book: "My new book 15"
+      book: "My new book 1"
     }
-    request
+    const res = await request
       .post('/api/book')
       .send(body)
-      .expect(201)
-      .then(response => {
-        if (response.body.data) {
-          request
-            .delete('/api/book')
-            .send(deleteBody)
-            .expect(204)
-        }
-        done()
-      });
+    if (res.body.data) {
+      const deleteRequest = await request
+        .delete('/api/book')
+        .send(deleteBody)
+        .expect(204)
+    }
   });
 
-  it("should update a book title", (done) => {
+  it("should update a book title", async () => {
     const body = {
       original_book: 'Book1',
       new_book: 'My new title'
     }
-    request
+    const res = await request
       .patch('/api/book')
       .send(body)
       .expect(200)
-      .then(res => {
-        expect(res.body.status).toEql('Ok')
-      })
-    done()
+    expect(res.body.status).to.eql('Ok')
   });
 
-  it("should create a new book", (done) => {
+  it("should create a new book", async () => {
+    require('./../server').server;
     const body = {
-      book: "My new book 15"
+      book: "My new book"
     }
-    request
+    const res = await request
       .post('/api/book')
       .send(body)
-      .expect(201)
-      .then(response => {
-        expect(response.body.status).toBe('Ok');
-        expect(response.message).toEql('New book: "My new book 15" added successfully.')
-      })
-    done()
+    expect(res.body).to.haveOwnProperty('message')
+    expect(res.body.status).to.not.eql('Okay')
+    expect(res.body.status).to.eql('Ok')
   });
 
-  it("should save a book to the psuedo-database", (done) => {
+  it("should save a book to the psuedo-database", async () => {
     const body = {
       book: "My new book 1"
     }
-    request
+    const res = await request
       .put('/api/book')
       .send(body)
-      .expect(200)
-      .then(res => {
-        expect(res.body.message).toEql('Book has been updated.')
-        expect(res.body.data).to.haveOwnProperty('timeElapsed')
-        expect(res.body.data).to.haveOwnProperty('title')
-        expect(res.body.data.length).to.eql(1)
-      })
-    done()
+    expect(res.body.message).to.eql('Book has been updated.')
+    expect(res.body.data).to.be.an('array')
+    expect(res.body.data.length).to.eql(3)
   });
 });
